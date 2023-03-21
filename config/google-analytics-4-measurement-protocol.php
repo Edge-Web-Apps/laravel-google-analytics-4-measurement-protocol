aa191f3b514ac670460d6d7133a2025df23b927e
Bri MacBain <35091265+brimacbain@users.noreply.github.com>
2023-03-21 00:18:36 -0700

    Update google-analytics-4-measurement-protocol.php
    Added session_id

--- config/google-analytics-4-measurement-protocol.php
+++ config/google-analytics-4-measurement-protocol.php
@@ -4,6 +4,8 @@ return [
     'measurement_id' => env('MEASUREMENT_ID', null), # Same as Google Analytics Tracking ID
 
     'api_secret' => env('MEASUREMENT_PROTOCOL_API_SECRET', null),
+    
+    'ga4_session_id' => 'google-analytics-4-measurement-protocol.session_id',
 
     'client_id_session_key' => 'google-analytics-4-measurement-protocol.client_id'
 ];
