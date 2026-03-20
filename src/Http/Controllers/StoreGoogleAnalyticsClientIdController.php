58ed22ef683707b85da9ed5da3cb27c88aa858b1
Bri MacBain <35091265+brimacbain@users.noreply.github.com>
2026-03-20 14:38:39 -0700

    testing out more validation

--- src/Http/Controllers/StoreGoogleAnalyticsClientIdController.php
+++ src/Http/Controllers/StoreGoogleAnalyticsClientIdController.php
@@ -4,10 +4,17 @@ namespace Freshbitsweb\LaravelGoogleAnalytics4MeasurementProtocol\Http\Controlle
 
 class StoreGoogleAnalyticsClientIdController
 {
-    public function __invoke(): void
+    public function __invoke(StoreRequest $request)
     {
-        session([config('google-analytics-4-measurement-protocol.ga4_session_id') => request('session_id')]);
-        session([config('google-analytics-4-measurement-protocol.ga4_session_number') => request('session_number')]);
-        session([config('google-analytics-4-measurement-protocol.client_id_session_key') => request('client_id')]);
+        $validated = $request->validated();
+        $sessionId = $validated['session_id'] ?? null;
+        $sessionNumber = $validated['session_number'] ?? null;
+        $clientId = $validated['client_id'] ?? null;
+
+        Illuminate\Support\Facades\Log::info("GA4 Test: ".json_encode($validated));
+
+        session([config('google-analytics-4-measurement-protocol.ga4_session_id') => $sessionId]);
+        session([config('google-analytics-4-measurement-protocol.ga4_session_number') => $sessionNumber]);
+        session([config('google-analytics-4-measurement-protocol.client_id_session_key') => $clientId]);
     }
 }
---
+++ src/Http/Controllers/StoreRequest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Freshbitsweb\LaravelGoogleAnalytics4MeasurementProtocol\Http\Controllers;
+
+use App\Sanitizers\SanitizeHtmlPurifier;
+//use ArondeParon\RequestSanitizer\Sanitizers\RemoveNonNumeric;
+use App\Sanitizers\RemoveNonNumeric;
+use ArondeParon\RequestSanitizer\Traits\SanitizesInputs;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+
+
+class StoreRequest extends FormRequest
+{
+    //Adds trait that allows sanitizing inputs
+    use SanitizesInputs;
+
+    protected $sanitizers = [
+        'session_id' => [
+            RemoveNonNumeric::class
+        ],
+        'session_number' => [
+            RemoveNonNumeric::class
+        ],
+        'client_id' => [
+            RemoveNonNumeric::class
+        ],
+    ];
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // No auth check needed
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'session_id' => 'nullable|sometimes|numeric',
+            'session_number' => 'nullable|sometimes|numeric',
+            'client_id' => 'nullable|sometimes|numeric'
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
